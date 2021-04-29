59e8a7d759c80ab9c6910df8dda90e71f8b4cfe5
Mabel Lawrence <mabelmail3@gmail.com>
2021-04-29 17:19:00 -0400

    added hooks

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { Page } = require('../models');
-const { addPage}  = require('../views/addPage');
+const addPage  = require('../views/addPage');
 const router = express.Router();
 
 module.exports = router;
@@ -15,10 +15,42 @@ router.post('/', async (req, res, next) => {
     const email = req.body.email;
     const status = req.body.status;
     const content = req.body.content;
+    // const slug = createSlug(title);
 
-    res.json(req.body);
+    Page.beforeValidate((page) => {
+        let slug = page.title;
+        if(!title){
+            //code to randomly generate title/slug
+            slug = Math.random.toString(16).substr(2,8);
+        }
+        //create slug from existing title
+        slug = slug.replace(/\s+/g,'_');
+        slug = slug.replace(/\W/g,'');
+        page.slug = slug;
+    });
+
+    try{
+        const page = await Page.create({
+            name,
+            email,
+            title,
+            content,
+            status
+        })
+        res.redirect('/');
+    }catch(error){
+        next(error);
+    }
 });
 
 router.get('/add', (req, res, next) => {
     res.send(addPage())
 });
+
+router.get('/:slug',(req,res,next) => {
+    try{
+        res.send(`hit dynamic route at ${req.params.slug}`)
+    } catch(error) {
+        next(error);
+    }
+})
